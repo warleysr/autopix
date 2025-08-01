707d61a5b4f21d9450bbcc008af1a09ecbbb5ce2
Warley <warleysoaresr@gmail.com>
2025-07-31 22:35:14 -0300

    refactor time manager for simplicity

--- src/io/github/warleysr/autopix/TimeManager.java
+++ src/io/github/warleysr/autopix/TimeManager.java
@@ -7,9 +7,7 @@ import org.bukkit.entity.Player;
 
 public class TimeManager {
 	
-	private static final HashMap<String, Long> CREATE = new HashMap<>();
-	private static final HashMap<String, Long> LIST = new HashMap<>();
-	private static final HashMap<String, Long> VALIDATE = new HashMap<>();
+	private static final HashMap<String, HashMap<String, Long>> DELAY = new HashMap<>();
 	
 	public static boolean canExecute(AutoPix ap, Player p, String command) {
 		HashMap<String, Long> map = null;
@@ -17,28 +15,33 @@ public class TimeManager {
 		
 		switch (command) {
 		case "create":
-			map = CREATE;
 			delay = ap.getConfig().getInt("tempos.criar-pedido");
 			break;
 		case "list":
-			map = LIST;
 			delay = ap.getConfig().getInt("tempos.lista");
 			break;
 		case "validate":
-			map = VALIDATE;
 			delay = ap.getConfig().getInt("tempos.validar");
+			break;
+		case "cancel":
+			delay = ap.getConfig().getInt("tempos.cancelar");
+			break;
+		case "top":
+			delay = ap.getConfig().getInt("tempos.top");
 			break;
 		default:
 			return true;
 		}
 		long time = System.currentTimeMillis();
 		
-		if (map.containsKey(p.getName())) {
-			long diff = time - map.get(p.getName());
+		if (DELAY.containsKey(p.getName())) {
+			map = DELAY.get(p.getName());
+			
+			long diff = time - map.getOrDefault(command, 0L);
 			long seconds = TimeUnit.MILLISECONDS.toSeconds(diff);
 			
 			if (seconds >= delay) {
-				map.put(p.getName(), time);
+				map.put(command, time);
 				return true;
 			}
 			
@@ -46,7 +49,9 @@ public class TimeManager {
 			return false;
 		}
 		else {
-			map.put(p.getName(), time);
+			map = new HashMap<String, Long>();
+			map.put(command, time);
+			DELAY.put(p.getName(), map);
 			return true;
 		}
 	}
