8b4aac91f7c9c640efc4419d3407174a2c471b0f
Warley <warleysoaresr@gmail.com>
2025-08-09 15:01:37 -0300

    feat: allow using HEX colors in Paper

--- src/io/github/warleysr/autopix/MSG.java
+++ src/io/github/warleysr/autopix/MSG.java
@@ -7,10 +7,14 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.md_5.bungee.api.ChatColor;
+
 public class MSG {
 	
 	private static final HashMap<String, String> MESSAGES = new HashMap<>();
-	
 	
 	protected static void loadMessages(AutoPix ap) {
 		File msgFile = new File(ap.getDataFolder(), "mensagens.yml");
@@ -23,7 +27,7 @@ public class MSG {
 			
 			if (msg.isList(key)) {
 				for (String line : msg.getStringList(key))
-					message += line.replace('&', '\u00a7') + "\n";
+					message += translateColors(line) + "\n";
 			}
 			else
 				message = msg.getString(key).replace('&', '\u00a7');
@@ -39,5 +43,47 @@ public class MSG {
 	public static String getMessage(String key) {
 		return MESSAGES.get(key);
 	}
-
+	
+	private static boolean isPaper() {
+	    try {
+	        Class.forName("com.destroystokyo.paper.PaperConfig");
+	        return true;
+	    } catch (ClassNotFoundException e) {
+	        return false;
+	    }
+	}
+	
+	private static String translateColors(String message) {
+		if (!isPaper())
+			return ChatColor.translateAlternateColorCodes('&', message);
+		
+		message = legacyToMiniMessage(message);
+		Component component = MiniMessage.miniMessage().deserialize(message);
+		return LegacyComponentSerializer.legacySection().serialize(component);
+	}
+	
+    private static String legacyToMiniMessage(String legacyText) {
+        return legacyText
+            .replace("&0", "<black>")
+            .replace("&1", "<dark_blue>")
+            .replace("&2", "<dark_green>")
+            .replace("&3", "<dark_aqua>")
+            .replace("&4", "<dark_red>")
+            .replace("&5", "<dark_purple>")
+            .replace("&6", "<gold>")
+            .replace("&7", "<gray>")
+            .replace("&8", "<dark_gray>")
+            .replace("&9", "<blue>")
+            .replace("&a", "<green>")
+            .replace("&b", "<aqua>")
+            .replace("&c", "<red>")
+            .replace("&d", "<light_purple>")
+            .replace("&e", "<yellow>")
+            .replace("&f", "<white>")
+            .replace("&l", "<bold>")
+            .replace("&m", "<strikethrough>")
+            .replace("&n", "<underlined>")
+            .replace("&o", "<italic>")
+            .replace("&r", "<reset>");
+    }
 }
